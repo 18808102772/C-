b1e431d38ec4e390be8d1f0a97c14add1363067c
18808102772 <3441876909@qq.com>
2022-09-12 19:34:47 +0800

    上传

--- text/从两端向中间打印/从两端向中间打印/text.c
+++ text/从两端向中间打印/从两端向中间打印/text.c
@@ -2,11 +2,29 @@
 #include<string.h>
 #include<windows.h>
 #include<stdlib.h>
+//÷∏’Î”√∑®
 int main()
 {
 	char a1[] = "welcome to bit!!!!!!!";
 	char a2[] = "                     ";
 	int left = 0;
+	int right = strlen(a1) - 1;
+	while (left <= right)
+	{
+		int* p1 = &a1[left];
+		int* p2 = &a2[left];
+		int* p3 = &a1[right];
+		int* p4 = &a2[right];
+		*p2 = *p1;
+		*p4 = *p3;
+		printf("%s\n", a2);
+		Sleep(1000);
+		system("cls");
+		left++;
+		right--;
+	}
+
+	/*int left = 0;
 	int right = strlen(a1)-1;
 	while (left <= right)
 	{
@@ -17,6 +35,6 @@ int main()
 		system("cls");
 		left++;
 		right--;
-	}
+	}*/
 	return 0;
 }
