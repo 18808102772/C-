c2ac6117c16e379657b8e692abfbfe5baecb5fc6
18808102772 <3441876909@qq.com>
2022-11-04 00:48:16 +0800

    上传

--- text/扫雷/扫雷/game.c
+++ text/扫雷/扫雷/game.c
@@ -47,19 +47,30 @@ void setmine(char board[ROWS][COLS], int row, int col)
 	}
 
 }
+//'1' - '0' = 1
+//'0' - '0' = 0
+//数组里面放的是字符，需要显示的是数字
 int get_mine_count(char mine[ROWS][COLS], int x, int y)
 {
-
+	return mine[x - 1][y] +
+		mine[x - 1][y + 1] +
+		mine[x][y + 1] +
+		mine[x + 1][y + 1] +
+		mine[x + 1][y] +
+		mine[x + 1][y - 1] +
+		mine[x][y - 1] +
+		mine[x - 1][y - 1] - 8 * '0';
 
 }
 void findmine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col)
 {
 	int x = 0;
 	int y = 0;
-	while (1)
+	int win = 0;//共有9*9-10个雷
+	while (win <= row * col - easy_count)
 	{
 		printf("请输入排查雷的坐标：>");
-		scanf("%d%d", &x, &y);
+		scanf_s("%d%d", &x, &y);
 		if (x >= 1 && x <= row && y >= 1 && y <= col)
 		{
 			//坐标合法
@@ -75,12 +86,20 @@ void findmine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col)
 			{
 				//计算x，y周围雷的个数
 				int count = get_mine_count;
+				show[x][y] = count + '0';
+				displayboard(show, row, col);
+				win++;
 			}
 		}
 		else
 		{
 			printf("输入错误，请重新输入！\n");
+
 		}
 	}
-
+	if (win == row * col - easy_count)
+	{
+		printf("恭喜你，扫雷成功");
+		displayboard(mine, row, col);
+	}
 }
